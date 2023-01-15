3b4456fa1f9aee302908272dbf6d2039ca0d1207
Dnyaneshwar Dongle <ddm.dongle@gmail.com>
2023-01-15 22:49:23 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+//        System.out.println(obj.name);
+//        C:\Users\ddong\encapsulation-DDongleGithub\src\main\java\com\driver\Main.java:6:31
+//        java: name has private access in com.driver.RWOnly
+        obj.setName("DD");
+        obj.getName();
+    }
   
 }
